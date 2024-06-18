eca08e6a4b5047e22feee764bca54941e44c5bf0
jiwon <pooojw@naver.com>
2024-06-18 23:32:26 +0900

    chore: fontawesome 라이브러리 추가 및 설정

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(fas);
+library.add(far);
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+// createApp을 통해 생성한Application 인스턴스의 component API 활용
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
