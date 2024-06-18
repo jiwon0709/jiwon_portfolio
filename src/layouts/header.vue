f8bb25ebc0a7e373fb8d54eadf8c66cbd0809c85
jiwon <pooojw@naver.com>
2024-06-18 19:12:54 +0900

    feat: header 추가

---
+++ src/layouts/header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <ul>
+      <li>Home</li>
+      <li>About me</li>
+      <li>Skills</li>
+      <li>Career</li>
+      <li>Capability</li>
+    </ul>
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+header {
+  position: fixed;
+  width: 100%;
+  height: auto;
+  background: white;
+  font-size: 1.5rem;
+  text-align: center;
+}
+ul li {
+  display: inline-block;
+  padding: 1.5rem 2rem;
+}
+</style>
