b11818ef978cb1f4fb54b436dabe1464fa139c7c
jiwon <pooojw@naver.com>
2024-06-26 22:55:43 +0900

    feat: header 기능 추가

--- src/layouts/header.vue
+++ src/layouts/header.vue
@@ -1,16 +1,31 @@
 <template>
-  <header>
-    <ul>
-      <li>Home</li>
-      <li>About me</li>
-      <li>Skills</li>
-      <li>Career</li>
-      <li>Capability</li>
+  <header class="header">
+    <ul @click="menuClick">
+      <li data-menu="mainWrap">Home</li>
+      <li data-menu="aboutMeWrap">About me</li>
+      <li data-menu="skillsWrap">Skills</li>
+      <li data-menu="careerWrap">Career</li>
+      <li data-menu="capaWrap">Capability</li>
     </ul>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+const menuClick = (event) => {
+  const menuNm = event.target.closest("li").getAttribute("data-menu");
+
+  if (menuNm) {
+    scrollToMenu(menuNm);
+  }
+};
+
+const scrollToMenu = (menuNm) => {
+  const menuHeight = document.querySelector(".header").offsetHeight;
+  const topLocation =
+    document.querySelector("." + menuNm).offsetTop - menuHeight;
+  window.scrollTo({ top: topLocation, behavior: "smooth" });
+};
+</script>
 
 <style lang="scss" scoped>
 header {
@@ -22,9 +37,12 @@ header {
   text-align: center;
   opacity: 0.9;
   box-shadow: 0 0 0.5rem hsl(0deg 0% 75% / 80%);
+  z-index: 999;
 }
 ul li {
   display: inline-block;
   padding: 1.5rem 2rem;
+  font-weight: 500;
+  cursor: pointer;
 }
 </style>
